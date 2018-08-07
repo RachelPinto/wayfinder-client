a543f204d8ebfc975290e5f9212ee175bb178781
rpinto <rachel.lea.pinto@gmail.com>
2018-08-07 09:36:52 -0700

    began adding static logging form

---
+++ src/components/log-form.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+import './styles/log-form.css';
+
+export default function Logform() {
+    return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import Logform from './components/log-form';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Logform />, 
+  document.getElementById('root')
+);
 registerServiceWorker();
