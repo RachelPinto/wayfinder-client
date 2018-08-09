54689904713baceccc777a95fc6dadaa3c8f28ae
rpinto <rachel.lea.pinto@gmail.com>
2018-08-09 11:31:10 -0700

    symptom check submitting

--- src/components/symptom-array.js
+++ src/components/symptom-array.js
@@ -6,19 +6,18 @@ import { Link } from 'react-router-dom';
 import { experiencedForm } from '../actions'
 import { connect } from 'react-redux'
 
-export class Symptomarray extends React.Component() {
-  constructor(props) {
-    super(props);
-    this.state = {
-    }
-    this.onClick = this.onClick.bind(this);
-  }
+export class Symptomarray extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //   }
+  //   this.onClick = this.onClick.bind(this);
+  // }
 
-
-  onClick(event) {
-    event.preventDefault();
+  handleChecked(event) {
+    //event.preventDefault();
     let experienceFormData = {
-      experience: event.currentTarget.experience.value,
+      experience: this.refs.checking.checked
     }
     this.props.dispatch(experiencedForm(experienceFormData))
     console.log(experienceFormData)
@@ -28,13 +27,20 @@ export class Symptomarray extends React.Component() {
       <div>
         <h1>Let's check in</h1>
         <p>lorem ipsum</p>
-
         <div>
           <Link to="/log-form">
             <button>Symptom 1</button>
           </Link>
-          <label htmlFor="experienced" onClick={(e) => this.onClick(e)}>Experienced?</label>
-          <input type="checkbox" id="experienced" name="experienced" value="1"></input>
+          <div>
+            <label htmlFor="experienced">Experienced?</label>
+            <input 
+              type="checkbox" 
+              id="experienced" 
+              name="experienced" 
+              ref="checking" 
+              onChange={() => this.handleChecked()}>
+            </input>
+          </div>
         </div>
 
       </div>
@@ -42,3 +48,4 @@ export class Symptomarray extends React.Component() {
   }
 }
 export default connect()(Symptomarray);
+
