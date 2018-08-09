835ebde2f1deb0430c56a4f55cb9190adf5531bb
rpinto <rachel.lea.pinto@gmail.com>
2018-08-09 09:20:25 -0700

    updated client side with new experience form

--- src/components/symptom-array.js
+++ src/components/symptom-array.js
@@ -1,40 +1,52 @@
-import React from 'react';
+import React from '../../../../Library/Caches/typescript/2.9/node_modules/@types/react';
 
 import './styles/symptom-array.css';
-import { Link } from 'react-router-dom';
 
-export default function Symptomarray() {
-  return (
+import { Link } from '../../../../Library/Caches/typescript/2.9/node_modules/@types/react-router-dom';
+import { experiencedForm } from '../actions'
+import { connect } from '../../../../Library/Caches/typescript/2.9/node_modules/@types/react-redux'
+
+export class Symptomarray extends React.Component() {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+    this.onClick = this.onClick.bind(this);
+  }
+
+
+  onClick(event) {
+    event.preventDefault();
+    let experienceFormData = {
+      experience: event.currentTarget.experience.value,
+    }
+    this.props.dispatch(logForm(experienceFormData))
+    console.log(experienceFormData)
+  }
+  render() {
+    return (
       <div>
         <h1>Let's check in</h1>
         <p>lorem ipsum</p>
+
         <div>
-        <Link to="/log-form">
-          <button>Symptom 1</button>
-        </Link>
+          <Link to="/log-form">
+            <button>Symptom 1</button>
+          </Link>
           <label htmlFor="experienced">Experienced?</label>
-
           <input type="checkbox" id="experienced" name="experienced" value="1"></input>
-
         </div>
+
         <div>
-        <Link to="/log-form">
-          <button>Symptom 2</button>
-        </Link>
-          <label htmlFor="experienced">Experienced?</label>
-
+          <Link to="/log-form">
+            <button>Symptom 2</button>
+          </Link>
+          <label htmlFor="experienced" onClick={(e) => this.onClick(e)}>Experienced?</label>
           <input type="checkbox" id="experienced" name="experienced" value="1"></input>
-
         </div>
-        <div>
-        <Link to="/log-form">
-          <button>Symptom 3</button>
-        </Link>
-          <label htmlFor="experienced">Experienced?</label>
 
-          <input type="checkbox" id="experienced" name="experienced" value="1"></input>
-
-        </div>       
       </div>
-  );
-};
+    )
+  }
+}
+export default connect()(Symptomarray);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,9 +12,7 @@ const initialState = {
 };
 
 export const wayfinderReducer = (state=initialState, action) => {
-    // if (action.type === actions.LOG_FORM) {
-    //     return Object.assign({}, state, {symptoms:{depression:action.formData}})
-    // }
+
     if (action.type === 'logFormRequest') {
         return Object.assign({}, state, {loading:true})
     }
@@ -30,5 +28,22 @@ export const wayfinderReducer = (state=initialState, action) => {
     if (action.type === 'logFormError') {
         return Object.assign({}, state, {error: action.error})
     }
+
+    if (action.type === 'experiencedRequest') {
+        return Object.assign({}, state, {loading:true})
+    }
+    if (action.type === 'experiencedSuccess') {
+        return Object.assign({}, state, {
+           symptoms: Object.assign({}, state.symptoms, {
+                depression: action.payload.depression,
+                anxiety: action.payload.anxiety,
+                insomnia: action.payload.insomnia
+           }), loading:false
+        })
+    }
+    if (action.type === 'experiencedError') {
+        return Object.assign({}, state, {error: action.error})
+    }
+
     return state;
 };
